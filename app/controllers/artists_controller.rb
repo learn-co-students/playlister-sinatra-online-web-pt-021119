ee59ef5a06d9b9112b678024dff27c1c9545341d
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-06-10 06:36:04 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,9 +1,10 @@
 require 'rack-flash'
 
 class ArtistsController < ApplicationController
-  use Rack::Flash
+  #use Rack::Flash
 
   get '/artists' do
+    @artists = Artist.all
     erb :'/artists/index'
   end
 end
--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,9 +1,10 @@
 require 'rack-flash'
 
 class GenresController < ApplicationController
-  use Rack::Flash
+  #use Rack::Flash
 
   get '/genres' do
+    @genres = Genre.all
     :'/genres/index'
   end
 
